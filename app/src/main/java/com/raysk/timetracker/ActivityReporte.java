a14d86552c155c27459c4ec98278305738159f91
EfrenJmnz <125478630+EfrenJmnz@users.noreply.github.com>
2023-06-04 19:01:04 -0700

    Se añadio un intent para que al presionar "Enviar" se abra una aplicacion para enviar correos electronicos.
      - Comprueba que ambos campos esten llenos, de lo contrario manda un toast (Puede ser cambiado a Toasty).
      - El destinatario debería ser el correo del administrador. Este se puede asignar modificando el contenido del String "mail".
      - Revisar el Manifest. En caso de ser necesario quitar que la app se abra en el ActivityReporte.

--- app/src/main/java/com/raysk/timetracker/ActivityReporte.java
+++ app/src/main/java/com/raysk/timetracker/ActivityReporte.java
@@ -3,11 +3,13 @@ package com.raysk.timetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class ActivityReporte extends AppCompatActivity {
     private EditText ETTema;
@@ -17,16 +19,32 @@ public class ActivityReporte extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reporte);
 
-        ETTema.findViewById(R.id.ETTema);
-        ETContenido.findViewById(R.id.ETContenido);
+        ETTema = findViewById(R.id.ETTema);
+        ETContenido = findViewById(R.id.ETContenido);
+        BTNEnviar = findViewById(R.id.BTNEnviar);
 
         BTNEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-              //  Intent intent = new intent(MainActivity.this, ().class);
-              //  startActivity(intent);
+            public void onClick(View v) {
+                String bodyMail = ETContenido.getText().toString();
+                // Aqui debe de ir el correo del usuario
+                String mail = "Correo@gmail.com";
+                String subject = ETTema.getText().toString();
+
+                if (bodyMail.isEmpty() || subject.isEmpty()) {
+                    Toast.makeText(ActivityReporte.this, "Por favor, llene todos los campos", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Intent intentMail = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + mail));
+                    Intent intentMailFull = new Intent(Intent.ACTION_SEND);
+                    intentMailFull.setType("message/rfc822");
+                    intentMailFull.putExtra(Intent.EXTRA_SUBJECT, subject);
+                    intentMailFull.putExtra(Intent.EXTRA_TEXT, bodyMail);
+                    intentMailFull.putExtra(Intent.EXTRA_EMAIL, new String[]{mail, mail});
+                    startActivity((intentMailFull));
+                }
             }
         });
 
