d00d11122c7f8372fb94ff23e4402bf58e935399
EfrenJmnz <125478630+EfrenJmnz@users.noreply.github.com>
2023-06-04 20:47:48 -0700

    Se agregaron archivos en las carpetas layout, app y drawable para el modulo de configuración.
     - Se desconoce si tiene funcionabilidad.
     - El activity principal tiene intents, pero la unica forma de regresar al anterior es hacerlo manualmente con el boton de regresar del teléfono.
     - Se MODIFICO el MANIFEST para agregar los activities nuevos. FAVOR DE REVISAR.

---
+++ app/src/main/java/com/raysk/timetracker/ConfigurancionEMail.java
@@ -0,0 +1,14 @@
+package com.raysk.timetracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfigurancionEMail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurancion_email);
+    }
+}
---
+++ app/src/main/java/com/raysk/timetracker/configurancionnombre.java
@@ -0,0 +1,15 @@
+package com.raysk.timetracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class configurancionnombre extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurancionnombre);
+    }
+}
